46e1c523aea7187160d3a8590ff61225e248e7d4
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2024-10-27 23:26:07 -0600

    creating separate graph and nodes files

--- agent/state.py
+++ agent/state.py
@@ -1,19 +1,13 @@
-import os
 import operator
 
 from typing import Annotated
 
 from typing_extensions import TypedDict
 
-from langgraph.graph import StateGraph
 from langgraph.graph.message import add_messages
 from langchain_core.agents import AgentAction
 from langchain_core.messages import BaseMessage
-from langchain_openai import ChatOpenAI
-from langgraph.prebuilt import ToolNode, tools_condition
 
-from tools import equip_tooling
-from utils import  _visualize_graph
 
 class AgentState(TypedDict):
     """
@@ -43,56 +37,3 @@ class ChatbotState(TypedDict):
     State keys without an annotation will be overwritten by each update, storing the most recent value.
     """
     messages: Annotated[list, add_messages]
-
-
-def chatbot(state: ChatbotState):
-    """
-    Graph Node
-    Takes the current State as input and returns a dictionary containing an updated messages list
-    under the key "messages". This is the basic pattern for all LangGraph node functions.
-    """
-    llm_config = ChatOpenAI(
-        model=os.environ["AGENT_MODEL"],
-        api_key=os.environ["OPENAI_API_KEY"],
-        temperature=0,
-        verbose=True,
-        max_tokens=None,
-        timeout=None,
-        max_retries=2,
-        # allow_dangerous_code=True,
-    )
-
-    tools = equip_tooling()
-
-    llm = llm_config.bind_tools(tools)
-
-    return {"messages": [llm.invoke(state["messages"])]}
-
-
-def graph_state():
-    # Select which state definition to use
-    State = ChatbotState
-
-    graph_builder = StateGraph(State)
-    # The first argument is the unique node name
-    # The second argument is the function or object that will be called whenever the node is used.
-    graph_builder.add_node("chatbot", chatbot)
-
-    tools = equip_tooling()
-    tool_node = ToolNode(tools=tools)
-    graph_builder.add_node("tools", tool_node)
-
-    graph_builder.add_conditional_edges(
-        "chatbot",
-        tools_condition,
-    )
-    # Any time a tool is called, we return to the chatbot to decide the next step
-    graph_builder.add_edge("tools", "chatbot")
-    graph_builder.set_entry_point("chatbot")
-
-    # creates a "CompiledGraph" we can invoke on our state
-    graph = graph_builder.compile()
-    # outputs a mermaid diagram of the graph in png format
-    _visualize_graph(graph)
-
-    return graph
