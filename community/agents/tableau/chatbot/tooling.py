82effbd4df1b4a05671ca2dc0c1e8c2ae52fa92b
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2024-11-26 23:33:28 -0700

    adding query tableau data source to chatbot tooling

--- community/agents/tableau/chatbot/tooling.py
+++ community/agents/tableau/chatbot/tooling.py
@@ -1,25 +1,18 @@
 from community.tools.others import llamaindex_pinecone_retriever, tavily_tool
+from community.tools.tableau.query_data import QueryTableauData
 
 
 def equip_tooling():
     # Knowledge Base tool
     knowledge_base = llamaindex_pinecone_retriever
 
-    # Example usage of query_data tool
-    # tool = QueryTableauData()
-    # result = tool.invoke({
-    #     "api_key": "your_api_key",
-    #     "datasource_id": "your_datasource_id",
-    #     "metadata": {"key": "value"},
-    #     "endpoint": "https://api.tableau.com/query",
-    #     "query": "How many rows are in table1?"
-    # })
-    # print(result)
+    # Tableau Data Source Query Tool
+    query_datasource = QueryTableauData
 
     # Web Search tool
     web_search = tavily_tool()
 
     # List of tools used to build the state graph and for binding them to nodes
-    tools = [knowledge_base, web_search]
+    tools = [knowledge_base, query_datasource, web_search]
 
     return tools
