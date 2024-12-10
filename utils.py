dd06245955e09fc0e58540a32ceb31e9365f5b8e
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2024-12-10 00:07:23 -0700

    dynamic kwargs for resusable Tableau authentication function

--- utils.py
+++ utils.py
@@ -32,15 +32,15 @@ async def http_post(endpoint, headers, payload):
             return responseObject
 
 # authenticates user to Tableau
-async def authenticate():
+async def authenticate_tableau_user(**kwargs):
     # Encode the payload and secret key to generate the JWT
     token = jwt.encode(
         {
-        "iss": os.environ['TABLEAU_JWT_CLIENT_ID'],
+        "iss": kwargs['TABLEAU_JWT_CLIENT_ID'],
         "exp": datetime.now(timezone.utc) + timedelta(minutes=5),
         "jti": str(uuid4()),
         "aud": "tableau",
-        "sub": os.environ['TABLEAU_USER'],
+        "sub": kwargs['TABLEAU_USER'],
         "scp": [
             "tableau:content:read",
             "tableau:insights:read",
@@ -49,16 +49,16 @@ async def authenticate():
             "tableau:metric_subscriptions:read",
         ]
         },
-        os.environ['TABLEAU_REST_JWT_SECRET'],
+        kwargs['TABLEAU_REST_JWT_SECRET'],
         algorithm = "HS256",
         headers = {
-        'kid': os.environ['TABLEAU_REST_JWT_SECRET_ID'],
-        'iss': os.environ['TABLEAU_JWT_CLIENT_ID']
+        'kid': kwargs['TABLEAU_REST_JWT_SECRET_ID'],
+        'iss': kwargs['TABLEAU_JWT_CLIENT_ID']
         }
     )
 
     # authentication endpoint + request headers & payload
-    endpoint = f"{os.environ['TABLEAU_DOMAIN']}/api/{os.environ['TABLEAU_API']}/auth/signin"
+    endpoint = f"{kwargs['TABLEAU_DOMAIN']}/api/{kwargs['TABLEAU_API']}/auth/signin"
 
     headers = {
         'Content-Type': 'application/json',
@@ -69,7 +69,7 @@ async def authenticate():
         "credentials": {
         "jwt": token,
         "site": {
-            "contentUrl": os.environ['TABLEAU_SITE'],
+            "contentUrl": kwargs['TABLEAU_SITE'],
         }
         }
     }
