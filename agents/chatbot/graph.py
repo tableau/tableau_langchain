6204eb02cbd79ec00ea1385aeacfe7c0d3afe79a
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2024-12-17 13:00:03 -0700

    defining tool node in nodes.py

--- agents/chatbot/graph.py
+++ agents/chatbot/graph.py
@@ -1,30 +1,27 @@
 from langgraph.graph import StateGraph
-from langgraph.prebuilt import ToolNode, tools_condition
+from langgraph.prebuilt import tools_condition
 
-from agents.chatbot.tooling import equip_tooling
 from agents.utils import  _visualize_graph
 
 
-def graph_state(state_definition, entry_point, node):
+def graph_state(state_definition, entry_point_name, entry_point_node, tool_node):
     # Select which state definition to use
     State = state_definition
 
     graph_builder = StateGraph(State)
     # The first argument is the unique node name
     # The second argument is the function or object that will be called whenever the node is used.
-    graph_builder.add_node(entry_point, node)
+    graph_builder.add_node(entry_point_name, entry_point_node)
 
-    tools = equip_tooling()
-    tool_node = ToolNode(tools=tools)
     graph_builder.add_node("tools", tool_node)
 
     graph_builder.add_conditional_edges(
-        entry_point,
+        entry_point_name,
         tools_condition,
     )
     # Any time a tool is called, we return to the chatbot to decide the next step
-    graph_builder.add_edge("tools", entry_point)
-    graph_builder.set_entry_point(entry_point)
+    graph_builder.add_edge("tools", entry_point_name)
+    graph_builder.set_entry_point(entry_point_name)
 
     # creates a "CompiledGraph" we can invoke on our state
     graph = graph_builder.compile()
