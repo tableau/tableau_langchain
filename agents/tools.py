0a38737f9eaa58779c6d4938c4fc1b5e0217ecee
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2025-01-04 00:59:30 -0700

    defining individual RAG tools for different topics -> metrics, data sources and workbooks

--- agents/tools.py
+++ agents/tools.py
@@ -20,13 +20,143 @@ def tavily_tool():
     return tavily
 
 
-@tool("llamaindex_pinecone_retriever")
-def llamaindex_pinecone_retriever(query: str):
+@tool("Tableau Pulse Metrics")
+def tableau_metrics(query: str):
     """
-    Finds specialist information from the organization's knowledge base using a natural language query.
+    Query a vector database for relevant information to the user query regarding
+    how their metrics, KPIs or OKRs are performing. This reference contains machine
+    learning insights use to provide up to date and reliable data regarding all kinds
+    of aspects related to the user's subscribed metrics
+
+    Output is various chunks of text in vector format for summarization.
+
+    Args:
+        query (str): A natural language query describing the data to retrieve or an open-ended question
+        that can be answered using information contained in the data source
+
+    Returns:
+        dict: A data set relevant to the user's query
     """
     pinecone_api_key = os.environ["PINECONE_API_KEY"]
-    index_name = os.environ["PINECONE_INDEX_NAME"]
+    index_name = os.environ["PINECONE_METRICS_INDEX"]
+    index_region = os.environ["PINECONE_ENVIRONMENT"]
+
+    # Initialize connection to Pinecone
+    pc = PineconeGRPC(api_key=pinecone_api_key)
+
+    # search for matching index in list of available indexes to client
+    existing_indexes = [index_info["name"] for index_info in pc.list_indexes()]
+    # if the index does not exist, create one - no matches is better than broken tools
+    if index_name not in existing_indexes:
+        pc.create_index(
+            name=index_name,
+            dimension=1536,
+            metric="cosine",
+            spec=ServerlessSpec(cloud="aws", region=index_region),
+        )
+        while not pc.describe_index(index_name).status["ready"]:
+            time.sleep(1)
+
+    # Initialize your index
+    pinecone_index = pc.Index(index_name)
+
+    # Initialize VectorStore
+    vector_store = PineconeVectorStore(pinecone_index=pinecone_index)
+
+    # Instantiate VectorStoreIndex object from your vector_store object
+    vector_index = VectorStoreIndex.from_vector_store(vector_store=vector_store)
+
+    # Grab 5 search results
+    retriever = VectorIndexRetriever(index=vector_index, similarity_top_k=5)
+
+    # Query vector DB
+    answer = retriever.retrieve(query)
+
+    # Inspect results
+    if (os.environ["DEBUG"] == 1):
+        print([i.get_content() for i in answer])
+
+    return answer
+
+
+@tool("Tableau Data Sources Catalog")
+def tableau_datasources(query: str):
+    """
+    Query a vector database to find the most relevant or useful Tableau data source to answer
+    the user query. Datasources will have descriptions and fields that may match the needs
+    of the user, use this information to determine the best data resource to target. Use this tool
+    if you must query a data source but you do not know which one to target.
+
+    Output is various chunks of text in vector format for summarization.
+
+    Args:
+        query (str): A natural language query describing the data to retrieve or an open-ended question
+        that can be answered using information contained in the data source
+
+    Returns:
+        dict: A data set relevant to the user's query
+    """
+    pinecone_api_key = os.environ["PINECONE_API_KEY"]
+    index_name = os.environ["PINECONE_DATASOURCES_INDEX"]
+    index_region = os.environ["PINECONE_ENVIRONMENT"]
+
+    # Initialize connection to Pinecone
+    pc = PineconeGRPC(api_key=pinecone_api_key)
+
+    # search for matching index in list of available indexes to client
+    existing_indexes = [index_info["name"] for index_info in pc.list_indexes()]
+    # if the index does not exist, create one - no matches is better than broken tools
+    if index_name not in existing_indexes:
+        pc.create_index(
+            name=index_name,
+            dimension=1536,
+            metric="cosine",
+            spec=ServerlessSpec(cloud="aws", region=index_region),
+        )
+        while not pc.describe_index(index_name).status["ready"]:
+            time.sleep(1)
+
+    # Initialize your index
+    pinecone_index = pc.Index(index_name)
+
+    # Initialize VectorStore
+    vector_store = PineconeVectorStore(pinecone_index=pinecone_index)
+
+    # Instantiate VectorStoreIndex object from your vector_store object
+    vector_index = VectorStoreIndex.from_vector_store(vector_store=vector_store)
+
+    # Grab 5 search results
+    retriever = VectorIndexRetriever(index=vector_index, similarity_top_k=5)
+
+    # Query vector DB
+    answer = retriever.retrieve(query)
+
+    # Inspect results
+    if (os.environ["DEBUG"] == 1):
+        print([i.get_content() for i in answer])
+
+    return answer
+
+
+@tool("Tableau Workbooks Catalog")
+def tableau_workbooks(query: str):
+    """
+    Query a vector database to find the most relevant or useful Tableau workbooks to answer
+    the user query. Workbooks contain multiple individual charts and dashboards with a description
+    that may match the needs of the user, use this information to recommend the best visual
+    analytics resource to explore.
+
+    Output is various chunks of text in vector format for summarization.
+
+    Args:
+        query (str): A natural language query describing the data to retrieve or an open-ended question
+        that can be answered using information contained in the data source
+
+    Returns:
+        dict: A data set relevant to the user's query
+    """
+    pinecone_api_key = os.environ["PINECONE_API_KEY"]
+    index_name = os.environ["PINECONE_WORKBOOKS_INDEX"]
     index_region = os.environ["PINECONE_ENVIRONMENT"]
 
     # Initialize connection to Pinecone
