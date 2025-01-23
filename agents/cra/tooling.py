40744968e6808d0f1e8c99b93d8b4cc6173d2da2
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2025-01-23 11:44:06 -0700

    new simple data source qa tool

--- agents/cra/tooling.py
+++ agents/cra/tooling.py
@@ -1,14 +1,16 @@
 from agents.tools import tableau_metrics, tavily_tool
 from community.langchain_community.tools.tableau.analyze_data import analyze_data
+from community.langchain_community.tools.tableau.datasource_qa import datasource_qa
+
 
 # Metrics RAG tool
 metrics = tableau_metrics
 
 # Tableau VizQL Data Service Query Tool
-analyze_datasource = analyze_data
+analyze_datasource = datasource_qa
 
 # Web Search tool
 # web_search = tavily_tool()
 
 # List of tools used to build the state graph and for binding them to nodes
-tools = [ metrics, analyze_datasource ]
+tools = [ metrics, datasource_qa ]
