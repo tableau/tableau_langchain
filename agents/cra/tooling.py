67eb4a14feedf8fd7894ee442218360fc97631c7
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2025-01-24 17:59:32 -0700

    initialization wrapper

--- agents/cra/tooling.py
+++ agents/cra/tooling.py
@@ -1,16 +1,44 @@
+import os
+from dotenv import load_dotenv
+
 from agents.tools import tableau_metrics, tavily_tool
+
 from community.langchain_community.tools.tableau.analyze_data import analyze_data
-from community.langchain_community.tools.tableau.datasource_qa import datasource_qa
+from community.langchain_community.tools.tableau.datasource_qa import datasource_qa, initialize_datasource_qa
+
+# Load environment variables before accessing them
+load_dotenv()
+
+tavily_key = os.environ['TAVILY_API_KEY']
+tableau_domain = os.environ['TABLEAU_DOMAIN']
+tableau_site = os.environ['TABLEAU_SITE']
+tableau_jwt_client_id = os.environ['TABLEAU_JWT_CLIENT_ID']
+tableau_jwt_secret_id = os.environ['TABLEAU_JWT_SECRET_ID']
+tableau_jwt_secret = os.environ['TABLEAU_JWT_SECRET']
+tableau_api_version = os.environ['TABLEAU_API_VERSION']
+tableau_user = os.environ['TABLEAU_USER']
+datasource_luid = os.environ['DATASOURCE_LUID']
+tooling_llm_model = os.environ['TOOLING_MODEL']
 
 
 # Metrics RAG tool
 metrics = tableau_metrics
 
 # Tableau VizQL Data Service Query Tool
-analyze_datasource = datasource_qa
+analyze_datasource = initialize_datasource_qa(
+    domain=tableau_domain,
+    site=tableau_site,
+    jwt_client_id=tableau_jwt_client_id,
+    jwt_secret_id=tableau_jwt_secret_id,
+    jwt_secret=tableau_jwt_secret,
+    tableau_api_version=tableau_api_version,
+    tableau_user=tableau_user,
+    datasource_luid=datasource_luid,
+    tooling_llm_model=tooling_llm_model
+)
 
 # Web Search tool
 # web_search = tavily_tool()
 
 # List of tools used to build the state graph and for binding them to nodes
-tools = [ metrics, datasource_qa ]
+tools = [ metrics, analyze_datasource ]
