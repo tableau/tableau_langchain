f4be801bab241933de42da0f9bb528bf850706ac
josephflu <899230+josephflu@users.noreply.github.com>
2025-02-14 18:49:30 -0600

    pip setup files - initial draft

--- pip_package/setup.py
+++ pip_package/setup.py
@@ -1,20 +1,20 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="langchain-tableau-test",  # Package name
-    version="0.1.4",    # Version
-    packages=find_packages(),  # Automatically find sub-packages
-    install_requires=[],  # List dependencies
-    author="Tableau Langchain Team",
-    author_email="tableau-langchain@tableau.com",
+    name="langchain_tableau",
+    version="0.2",
+    packages=find_packages(),
+    install_requires=[],
+    author="Tableau",
+    author_email="tab-se@tableau.com",
     description="Tableau tools for Agentic use cases with Langchain",
-    long_description=open("README.md").read(),
-    long_description_content_type="text/markdown",
-    url="https://github.com/Tab-SE/tableau_langchain",
+    license="MIT",
     classifiers=[
-        "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
     ],
-    python_requires=">=3.11",
+    include_package_data=True,
+    license_files=["LICENSE.txt"],
+    long_description_content_type="text/markdown",
+    long_description=open("README.md").read(),
 )
