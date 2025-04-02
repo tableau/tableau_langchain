527c1fdfb4112789cc5dc9fb7ccc67f7422ec04c
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2025-04-02 17:20:56 -0600

    added keynote agent

---
+++ experimental/agents/keynote/tooling.py
@@ -0,0 +1,37 @@
+import os
+from dotenv import load_dotenv
+
+from langchain_tableau.tools.simple_datasource_qa import initialize_simple_datasource_qa
+
+from experimental.agents.tools import tableau_metrics, tavily_tool
+from experimental.tools.datasource_qa import initialize_datasource_qa
+
+# Load environment variables before accessing them
+load_dotenv()
+tableau_domain = os.environ['TABLEAU_DOMAIN']
+tableau_site = os.environ['TABLEAU_SITE']
+tableau_jwt_client_id = os.environ['TABLEAU_JWT_CLIENT_ID']
+tableau_jwt_secret_id = os.environ['TABLEAU_JWT_SECRET_ID']
+tableau_jwt_secret = os.environ['TABLEAU_JWT_SECRET']
+tableau_api_version = os.environ['TABLEAU_API_VERSION']
+tableau_user = os.environ['TABLEAU_USER']
+tooling_llm_model = os.environ['TOOLING_MODEL']
+
+# Tableau VizQL Data Service Query Tool
+analyze_datasource = initialize_simple_datasource_qa(
+    domain=tableau_domain,
+    site=tableau_site,
+    jwt_client_id=tableau_jwt_client_id,
+    jwt_secret_id=tableau_jwt_secret_id,
+    jwt_secret=tableau_jwt_secret,
+    tableau_api_version=tableau_api_version,
+    tableau_user=tableau_user,
+    datasource_luid='a9623caf-c4e7-0297-fa31-83a5189dfad9',
+    tooling_llm_model=tooling_llm_model
+)
+
+# Web Search tool
+# web_search = tavily_tool()
+
+# List of tools used to build the state graph and for binding them to nodes
+tools = [ analyze_datasource ]
