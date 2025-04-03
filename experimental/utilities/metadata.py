6ac28ddc556be88a3db2ab1bc934c0b03bc4ee27
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2025-04-02 23:48:22 -0600

    importing from langchain_tableau

--- experimental/utilities/metadata.py
+++ experimental/utilities/metadata.py
@@ -1,7 +1,7 @@
 import json
 import requests
 from typing import Dict
-from experimental.utilities.utils import http_post
+from langchain_tableau.utilities.utils import http_post
 
 
 def get_datasource_query(luid):
