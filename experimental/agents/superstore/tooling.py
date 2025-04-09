ef3c65ccd84cfde86c1ecee0a09e9f52f20adf45
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2025-04-09 16:33:28 -0600

    updated workbooks to analytics

--- experimental/agents/superstore/tooling.py
+++ experimental/agents/superstore/tooling.py
@@ -30,5 +30,85 @@ analyze_datasource = initialize_simple_datasource_qa(
     tooling_llm_model=tooling_llm_model
 )
 
+tableau_metrics = pinecone_retriever_tool(
+    name='tableau_metrics',
+    description="""Returns ML insights & predictive analytics on user-subscribed metrics
+    Prioritize using this tool if the user mentions metrics, KPIs, OKRs or similar
+
+    Make thorough queries for relevant context.
+    Use "metrics update" for a summary. For detailed metric info, ask about:
+    - dimensions
+    - data
+    - descriptions
+    - drivers
+    - unusual changes
+    - trends
+    - sentiment
+    - current & previous values
+    - period over period change
+    - contributors
+    - detractors
+
+    NOT for precise data values. Use a data source query tool for specific values.
+    NOT for fetching data values on specific dates
+
+    Examples:
+    User: give me an update on my KPIs
+    Input: 'update on all KPIs, trends, sentiment"
+
+    User: what is going on with sales?
+    Input: 'sales trend, data driving sales, unusual changes, contributors, drivers and detractors'
+
+    User: what is the value of sales in 2024?
+    -> wrong usage of this tool, not for specific values
+    """,
+    pinecone_index=os.environ["METRICS_INDEX"],
+    model_provider=os.environ["MODEL_PROVIDER"],
+    embedding_model=os.environ["EMBEDDING_MODEL"]
+)
+
+tableau_datasources = pinecone_retriever_tool(
+    name='tableau_datasources_catalog',
+    description="""Find the most relevant or useful Tableau data sources to answer the user query. Datasources often
+    have descriptions and fields that may match the needs of the user, use this information to determine the best data
+    resource for the user to consult.
+
+    Output is various chunks of text in vector format for summarization.
+
+    Args:
+        query (str): A natural language query describing the data to retrieve or an open-ended question
+        that can be answered using information contained in the data source
+
+    Returns:
+        dict: A data set relevant to the user's query
+    """,
+    pinecone_index=os.environ["DATASOURCES_INDEX"],
+    model_provider=os.environ["MODEL_PROVIDER"],
+    embedding_model=os.environ["EMBEDDING_MODEL"]
+)
+
+tableau_analytics = pinecone_retriever_tool(
+    name='tableau_analytics_catalog',
+    description="""Find the most relevant or useful Tableau workbooks, dashboards, charts, reports and other forms
+    of visual analytics to help the user find canonical answers to their query. Unless the user specifically requests
+    for charts, workbooks, dashboards, etc. don't assume this is what they intend to find, if in doubt confirm by
+    letting them know you can search the catalog in their behalf.
+
+    If nothing matches the user's needs, then you might need to try a different approach such as querying a data source.
+
+    Output is various chunks of text in vector format for summarization.
+
+    Args:
+        query (str): A natural language query describing the data to retrieve or an open-ended question
+        that can be answered using information contained in the data source
+
+    Returns:
+        dict: A data set relevant to the user's query
+    """,
+    pinecone_index=os.environ["WORKBOOKS_INDEX"],
+    model_provider=os.environ["MODEL_PROVIDER"],
+    embedding_model=os.environ["EMBEDDING_MODEL"]
+)
+
 # List of tools used to build the state graph and for binding them to nodes
-tools = [ analyze_datasource ]
+tools = [ analyze_datasource, tableau_metrics, tableau_datasources, tableau_analytics ]
