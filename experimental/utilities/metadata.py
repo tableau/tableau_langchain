7f5882b280d587495a116f637695a357208e15e9
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2025-05-14 17:08:16 -0600

    handling metadata api errors

--- experimental/utilities/metadata.py
+++ experimental/utilities/metadata.py
@@ -22,6 +22,8 @@ def get_datasource_query(luid):
     }}
     """
 
+    print('query', query)
+
     return query
 
 
@@ -56,13 +58,9 @@ async def get_data_dictionary_async(api_key: str, domain: str, datasource_luid: 
 
 def get_data_dictionary(api_key: str, domain: str, datasource_luid: str) -> Dict:
     full_url = f"{domain}/api/metadata/graphql"
-
     query = get_datasource_query(datasource_luid)
 
-    payload = json.dumps({
-        "query": query,
-        "variables": {}
-    })
+    payload = json.dumps({"query": query, "variables": {}})
 
     headers = {
         'Content-Type': 'application/json',
@@ -71,21 +69,23 @@ def get_data_dictionary(api_key: str, domain: str, datasource_luid: str) -> Dict
     }
 
     response = requests.post(full_url, headers=headers, data=payload)
-    response.raise_for_status()  # Raise an exception for bad status codes
+    response.raise_for_status() # Raise an exception for bad status codes
 
-    json_data = response.json()['data']['publishedDatasources'][0]
+    data_sources = response.json().get('data', {}).get('publishedDatasources', [])
 
-    name = json_data.get('name')
-    description = json_data.get('description')
-    owner = json_data.get('owner', {}).get('name')
-    fields = [field for field in json_data.get('fields', []) if not field.get('isHidden')]
+    if not data_sources:  # Check for empty list
+        return {
+            'datasource_name': 'data source metadata such as names not available',
+            'datasource_description': 'data source metadata such as descriptions not available',
+            'datasource_owner': 'data source metadata such as data source owner not available',
+            'datasource_fields': 'data source metadata such as field descriptions and comments not available'
+        }
 
+    json_data = data_sources[0]  # Safely access first element
 
-    dictionary = {
-        'datasource_name': name,
-        'datasource_description': description,
-        'datasource_owner': owner,
-        'datasource_fields': fields,
+    return {
+        'datasource_name': json_data.get('name'),
+        'datasource_description': json_data.get('description'),
+        'datasource_owner': json_data.get('owner').get('name'),
+        'datasource_fields': [ field for field in json_data.get('fields', []) if not field.get('isHidden') ]
     }
-
-    return dictionary
