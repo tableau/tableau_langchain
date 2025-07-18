91dd11bb6c1dff136168df707aa5aa93bf887221
Allison Bierschenk <allisonreynoldsc@gmail.com>
2025-07-17 20:15:46 -0500

    new agent

--- experimental/agents/omnicell/tooling.py
+++ experimental/agents/omnicell/tooling.py
@@ -5,14 +5,14 @@ from langchain_tableau.tools.simple_datasource_qa import initialize_simple_datas
 
 # Load environment variables before accessing them
 load_dotenv()
-tableau_domain = os.environ['TABLEAU_DOMAIN']
-tableau_site = os.environ['TABLEAU_SITE']
-tableau_jwt_client_id = os.environ['TABLEAU_JWT_CLIENT_ID']
-tableau_jwt_secret_id = os.environ['TABLEAU_JWT_SECRET_ID']
-tableau_jwt_secret = os.environ['TABLEAU_JWT_SECRET']
-tableau_api_version = os.environ['TABLEAU_API_VERSION']
-tableau_user = os.environ['TABLEAU_USER']
-datasource_luid = os.environ['DATASOURCE_LUID']
+tableau_domain = os.environ['EACLOUD_TABLEAU_DOMAIN']
+tableau_site = os.environ['EACLOUD_TABLEAU_SITE']
+tableau_jwt_client_id = os.environ['EACLOUD_TABLEAU_JWT_CLIENT_ID']
+tableau_jwt_secret_id = os.environ['EACLOUD_TABLEAU_JWT_SECRET_ID']
+tableau_jwt_secret = os.environ['EACLOUD_TABLEAU_JWT_SECRET']
+tableau_api_version = os.environ['EACLOUD_TABLEAU_API_VERSION']
+tableau_user = os.environ['EACLOUD_TABLEAU_USER']
+datasource_luid = os.environ['OMNICELL_DATASOURCE_LUID']
 tooling_llm_model = os.environ['TOOLING_MODEL']
 
 # Tableau VizQL Data Service Query Tool
